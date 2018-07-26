82322def438646942ae4e866b529a94857940eb1
Renze Klamer <RKlamer@gmail.com>
2018-07-26 14:17:46 +0200

    code_day2-2

--- src/test/java/test/example/chapterSix.java
+++ src/test/java/test/example/chapterSix.java
@@ -10,8 +10,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class chapterSix {
-
     public void chapterSix() {
+    }
+
+
+    @Test
+    public void emptyCartTest() {
+
+        String account = "";
+        String urlTest = "techblog.polteq.com/testshop/index.php";
+        ChromeDriverManager.getInstance().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://" + urlTest);
+        driver.manage().window().maximize();
+        driver.findElement(By.className("login")).click();
+        driver.findElement(By.id("email")).sendKeys(new CharSequence[]{"renze.klamer@polteq.com"});
+        driver.findElement(By.id("passwd")).sendKeys(new CharSequence[]{"Mijn-1956"});
+        driver.findElement(By.id("SubmitLogin")).click();
+        account = driver.findElement(By.className("account")).getText();
+        ((AbstractCharSequenceAssert) Assertions.assertThat(account).as("user logged on", new Object[0])).contains(new CharSequence[]{"Renze"});
+        //boolean emptyCar = driver.findElement(By.id("ajax_cart_no_product")).isDisplayed();
+        //Assertions.assertThat(emptyCar).as("geenm lege car").isTrue();
+        driver.findElement(By.cssSelector("[class='tag_level3 first_item']")).click();
+        driver.findElement(By.cssSelector("[alt='iPod shuffle']")).click();
+        driver.findElement(By.cssSelector("[alt='iPod shuffle']")).click();
+
+//        Add to cart
+        //ajax_cart_no_product
     }
 
     @Test
@@ -32,15 +57,16 @@ public class chapterSix {
         boolean signoutExists = false;
         signoutExists = driver.findElement(By.className("logout")).isDisplayed();
         Assertions.assertThat(signoutExists).as("logdout not done").isTrue();
-        //driver.findElement(By.className("logout")).click();
+        driver.findElement(By.className("logout")).click();
         boolean signInExists = false;
         signInExists = driver.findElement(By.className("logout")).isDisplayed();
 
         Assertions.assertThat(signInExists).as("logout not done").isTrue();
 
         driver.quit();
-        System.out.println(account + " is het gelukt");
+        System.out.println(account + " is het gelukt" + signInExists + signoutExists);
     }
+
 }
 
 
