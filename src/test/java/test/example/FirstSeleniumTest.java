444a0adece2e00460369bd20da1c9a0e9f12abec
Renze Klamer <RKlamer@gmail.com>
2018-07-26 11:07:02 +0200

    first selenium

--- src/test/java/test/example/FirstSeleniumTest.java
+++ src/test/java/test/example/FirstSeleniumTest.java
@@ -22,7 +22,7 @@ public class FirstSeleniumTest {
         ChromeDriverManager.getInstance().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://" + urlTest);
-        urlFound = driver.getCurrentUrl().contains(urlTest);
+        urlFound = driver.getCurrentUrl();
 
         if (urlFound.contains(urlTest))
             siteFound = true;
@@ -37,7 +37,7 @@ public class FirstSeleniumTest {
         Assertions.assertThat(account).as("user logged on").contains("Renze");
 
         boolean signoutExists = false;
-        //signoutExists = driver.findElement()
+        signoutExists = driver.findElement(By.className("logout")).isDisplayed();
         //wait(30000);
         driver.quit();
         System.out.println(account + " is het gelukt");
